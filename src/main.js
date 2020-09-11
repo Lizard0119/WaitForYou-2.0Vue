1262c8cc1e8956d908e5e13eddfb6f0439e0be68
autumnmeaning <836203855@qq.com>
2020-09-11 16:42:20 +0800

    2020-09-11 16:41 autumn 依赖添加 version 1.0

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import Axios from 'axios'
+import VueCookie from 'vue-cookie'
+
+// import {ZegoExpressEngine} from 'zego-express-engine-webrtc'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = Axios;
+Vue.use(Vant);
+Vue.use(VueCookie)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
